31840c7e2676babfa288d57b307d4cec05c034d0
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2025-05-28 02:01:09 +0700

    feat login page

---
+++ mock/ecosystemModules.ts
@@ -0,0 +1,51 @@
+import iconNumberOne from '~/assets/icon/static/number-one.png'
+import iconNumberTwo from '~/assets/icon/static/number-2.png'
+import iconNumberThree from '~/assets/icon/static/number-3.png'
+import iconNumberFour from '~/assets/icon/static/number-four.png'
+import iconNumberFive from '~/assets/icon/static/number-5.png'
+import iconNumberSix from '~/assets/icon/static/six.png'
+import iconNumberSeven from '~/assets/icon/static/seven.png'
+import iconNumberEight from '~/assets/icon/static/number-8.png'
+
+export const ecosystemModules = [
+  {
+    icon: iconNumberOne,
+    title: 'Phần mềm quản lý bán vé',
+    route: '/test'
+  },
+  {
+    icon: iconNumberTwo,
+    title: 'Phần mềm quản lý hàng hoá',
+    route: '/test'
+  },
+  {
+    icon: iconNumberThree,
+    title: 'Phần mềm đại lý bán vé',
+    route: '/test'
+  },
+  {
+    icon: iconNumberFour,
+    title: 'App quản lý bán vé',
+    route: '/test'
+  },
+  {
+    icon: iconNumberFive,
+    title: 'App quản lý hàng hoá',
+    route: '/test'
+  },
+  {
+    icon: iconNumberSix,
+    title: 'App đại lý bán vé',
+    route: '/test'
+  },
+  {
+    icon: iconNumberSeven,
+    title: 'App tài xế đón/trả khách',
+    route: '/test'
+  },
+  {
+    icon: iconNumberEight,
+    title: 'Web/App bán vé thương hiệu nhà xe',
+    route: '/test'
+  },
+]
--- pages/index.vue
+++ pages/index.vue
@@ -1,56 +1,7 @@
 <script setup lang="ts">
 import logo from '@/assets/image/logo-4.png';
-import iconNumberOne from '~/assets/icon/static/number-one.png';
-import iconNumberTwo from '~/assets/icon/static/number-2.png';
-import iconNumberThree from '~/assets/icon/static/number-3.png';
-import iconNumberFour from '~/assets/icon/static/number-four.png';
-import iconNumberFive from '~/assets/icon/static/number-5.png';
-import iconNumberSix from '~/assets/icon/static/six.png';
-import iconNumberSeven from '~/assets/icon/static/seven.png';
-import iconNumberEight from '~/assets/icon/static/number-8.png';
 import LoginForm from '~/components/form/LoginForm.vue';
-const ecosystemModules = [
-    {
-        icon: iconNumberOne,
-        title: 'Phần mềm quản lý bán vé',
-        route: '/test'
-    },
-    {
-        icon: iconNumberTwo,
-        title: 'Phần mềm quản lý hàng hoá',
-        route: '/test'
-    },
-    {
-        icon: iconNumberThree,
-        title: 'Phần mềm đại lý bán vé',
-        route: '/test'
-    },
-    {
-        icon: iconNumberFour,
-        title: 'App quản lý bán vé',
-        route: '/test'
-    },
-    {
-        icon: iconNumberFive,
-        title: 'App quản lý hàng hoá',
-        route: '/test'
-    },
-    {
-        icon: iconNumberSix,
-        title: 'App đại lý bán vé',
-        route: '/test'
-    },
-    {
-        icon: iconNumberSeven,
-        title: 'App tài xế đón/trả khách',
-        route: '/test'
-    },
-    {
-        icon: iconNumberEight,
-        title: 'Web/App bán vé thương hiệu nhà xe',
-        route: '/test'
-    },
-];
+import { ecosystemModules } from '~/mock/ecosystemModules';
 function goToDetail(item: { route?: string }) {
     if (item.route) window.open(item.route, '_blank')
 }
@@ -109,7 +60,6 @@ function goToDetail(item: { route?: string }) {
                         Tối ưu vận hành - Nâng tầm thương hiệu - Kết nối khách hàng
                     </span>
                 </div>
-
             </div>
         </el-col>
     </el-row>
